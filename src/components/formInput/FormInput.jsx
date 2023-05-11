b3a4f6985826d7ae2874f8da460828d5e0188fed
zloibala <boronova25@gmail.com>
2023-05-10 19:32:58 -0500

    ...!

--- src/components/formInput/FormInput.jsx
+++ src/components/formInput/FormInput.jsx
@@ -10,6 +10,22 @@ const FormInput = ({ label, type, name }) => {
     }
     console.log(e.target.name, e.target.value);
   };
+  function fizzBuzz(n) {
+    if (n % 3 === 0 && n % 5 === 0) {
+      return "FizzBuzz";
+    } else if (n % 3 === 0) {
+      return "Fizz";
+    } else if (n % 5 === 0) {
+      return "Buzz";
+    } else {
+      return n;
+    }
+  }
+
+  console.log(fizzBuzz(3));
+  console.log(fizzBuzz(5));
+  console.log(fizzBuzz(15));
+  console.log(fizzBuzz(4));
 
   return (
     <div className="single-input">
